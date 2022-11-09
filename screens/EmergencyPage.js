4741a283ae20f216ab19f35334e88cb229e210e5
pDeepakPandey10 <pdeepakpandey10@gmail.com>
2022-11-09 23:20:01 +0530

    ok

--- screens/EmergencyPage.js
+++ screens/EmergencyPage.js
@@ -46,8 +46,6 @@ const EmergencyAppPage = (props) => {
                     return item.IncidentStatusId && item.StaffAssignedId != 0
                 });
 
-                console.log(isIncidentUnAssigned , ' ', isIncidentAssignedButPending);
-
                 if (isIncidentUnAssigned.length != 0) {
                     setIncidentRaisedModal(true);
                     getIncidentUpdate();
